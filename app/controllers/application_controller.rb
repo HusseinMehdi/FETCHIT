6cfdf934e70f01b6e46dface73e2fb36aa86733f
HusseinMehdi <hussein.mehdi@moguntia.com>
2024-03-11 10:03:44 +0100

    re-authentificate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,8 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user, :admin?
+  before_action :check_session_timeout
 
   private
-
-  # Fetches the current user from the session, if present
-  # def current_user
-  #   @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  # end
-  
-
-  # # Checks if the current user is an admin
-  # def admin?
-  #   current_user&.email == 'admin@moguntia.com'
-  # end
-
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id].present?
   end
@@ -21,7 +10,6 @@ class ApplicationController < ActionController::Base
   def admin?
     current_user&.email == 'admin@moguntia.com'
   end
-  
   
   # Ensures a user is logged in
   def require_login
@@ -36,6 +24,16 @@ class ApplicationController < ActionController::Base
     unless admin?
       flash[:alert] = "Nur der Administrator hat Zugriff auf diesen Bereich."
       redirect_to root_path # or wherever appropriate
+    end
+  end
+
+  def check_session_timeout
+    if current_user && session[:expires_at] && Time.current > session[:expires_at]
+      reset_session
+      flash[:alert] = "Your session has expired. Please log in again."
+      redirect_to root_path 
+    else
+      session[:expires_at] = 30.minutes.from_now
     end
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_fetchit_session', expire_after: 14.days
+Rails.application.config.session_store :cookie_store, key: '_fetchit_session', expire_after: 30.minutes
